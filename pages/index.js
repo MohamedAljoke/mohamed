2bc9cf1a8433806d062e3c36bd11a805d5321087
Mohamed Aljoke <mohammed.maljoke@gmail.com.br>
2023-08-09 21:44:38 -0300

    more changes

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Home() {
             </p>
             <p
               ref={textTwo}
-              className="tablet:text-6xl laptop:text-6xl laptopl:text-[70px] text-bold w-full laptop:w-4/5"
+              className="tablet:text-6xl laptop:text-6xl laptopl:text-[70px] font-semibold text-bold w-full laptop:w-4/5"
             >
               {data.headerTaglineTwo}
             </p>
@@ -90,9 +90,81 @@ export default function Home() {
           <Socials className="" />
         </div>
 
-        <div className="p-2 laptop:p-0" ref={workRef}>
-          <p className="mt-2 tablet:text-6xl laptop:text-6xl laptopl:text-[30px] text-bold w-full laptop:w-4/5">
-            Projects.
+        <div className="p-2 laptop:mt-20 mt-10" ref={workRef}>
+          <p className="tablet:text-6xl laptop:text-6xl laptopl:text-[30px] font-semibold w-full laptop:w-4/5">
+            Work Experience
+          </p>
+          <div className="mt-2">
+            <p className="text-[18px] font-bold mt-2">
+              IDIP Tecnologia (2022/10 - now)
+            </p>
+            <p className="text-lg laptop:text-lg laptop:w-4/5">
+              As a Fullstack Developer with expertise in Node.ts, React.ts,
+              Next.ts, NoSQL, and SQL, I have been actively engaged in diverse
+              projects since October 2022. My involvement spans across various
+              technology stacks including node.ts with Express, Adonis, and Nest
+              frameworks, all structured under DDD and MVC architectures. I have
+              adeptly contributed to microfrontend initiatives and harnessed the
+              power of Graphql and Hasura, employing Hasura actions for seamless
+              queries bridging both front and back-end domains. I've
+              successfully managed data transformations by facilitating the
+              import and export of CSV, JSON, and PDF files, while also
+              orchestrating efficient email delivery services. My commitment to
+              documentation has been paramount, demonstrated through meticulous
+              documentation using Postman and Swagger. Embracing the evolution
+              of technology, I've skillfully transitioned a JavaScript-based
+              back-end project to TypeScript, ensuring codebase integrity and
+              maintainability. My proficiency extends to comprehensive unit
+              testing across React front-end and Node back-end frameworks.
+              Moreover, I've innovatively crafted lambda functions utilizing the
+              principles of OOP and DDD, capitalizing on AWS's AppSync service.
+              My repertoire includes orchestrating the migration from Firebase
+              to MongoDB as the backend database for a project. I've also been
+              an integral part of a Next.js project that implements accelerated
+              mobile pages, as per the AMP specification. My toolkit encompasses
+              both Material UI and Tailwind, enabling me to design dynamic and
+              visually appealing user interfaces. To streamline project setup,
+              I've automated the creation of new back-end projects, integrating
+              Swagger and Docker for efficient database provisioning. Docker has
+              been instrumental in my endeavors, allowing me to encapsulate
+              Hasura and database instances within containers, thus optimizing
+              deployment processes and created gitlab piplines for continuous
+              integration. With a proven track record of adaptability and
+              innovation, I am poised to continue delivering impactful solutions
+              as a Fullstack Developer.
+            </p>
+          </div>
+          <div>
+            <p className="text-[18px] font-bold mt-2">
+              Skule Tecnologia (2023/03 - 2023/07)
+            </p>
+            <p className="text-lg laptop:text-lg laptop:w-4/5">
+              This role involves mentoring six programming interns, overseeing
+              their project utilizing Node.js and React, addressing merge
+              conflicts in pull requests, offering direction for code
+              enhancement. The mentoring sessions were conducted in English.
+            </p>
+          </div>
+          <div>
+            <p className="text-[18px] font-bold mt-2">
+              ADVANCED DIGITA (2022/02 - 2022/10)
+            </p>
+            <p className="text-lg laptop:text-lg laptop:w-4/5">
+              I utilized React.js in conjunction with Redux for crafting
+              e-commerce front-ends and ADMIN projects, seamlessly integrated
+              payment systems through custom React.js hooks for Mercado-Livre
+              and Cielo, developed two minimum viable product (MVP) applications
+              using Flutter (Dart) and GetX for state management, established
+              Node.js controllers, designed scripts for web scraping utilizing
+              Node.js, and effectively utilized SCSS and Bootstrap for styling
+              purposes.
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 laptop:mt-20 mt-10" ref={workRef}>
+          <p className="tablet:text-6xl laptop:text-6xl  font-semibold laptopl:text-[30px] text-bold w-full laptop:w-4/5">
+            Projects
           </p>
 
           <div className="grid grid-cols-1 tablet:grid-cols-2 gap-4 mt-2">
